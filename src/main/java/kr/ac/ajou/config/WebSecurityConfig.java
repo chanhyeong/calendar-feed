02375cf0bafa7706170e05783df6775d91935f6e
huy <huy@zoyi.co>
2016-12-11 22:55:08 +0900

    Fix gradle build for deploy.

--- src/main/java/kr/ac/ajou/config/WebSecurityConfig.java
+++ src/main/java/kr/ac/ajou/config/WebSecurityConfig.java
@@ -63,36 +63,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .and()
         .logout()
         .permitAll();
-//            .and().logout().logoutSuccessUrl("/users/sign_in").permitAll()
-//            .and().csrf().csrfTokenRepository(csrfTokenRepository())
-//            .and().addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
   }
-
-//  private Filter csrfHeaderFilter() {
-//    return new OncePerRequestFilter() {
-//      @Override
-//      protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-//                                      FilterChain filterChain) throws ServletException, IOException {
-//        CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
-//        if (csrf != null) {
-//          Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
-//          String token = csrf.getToken();
-//          if (cookie == null || token != null && !token.equals(cookie.getValue())) {
-//            cookie = new Cookie("XSRF-TOKEN", token);
-//            cookie.setPath("/social");
-//            response.addCookie(cookie);
-//          }
-//        }
-//        filterChain.doFilter(request, response);
-//      }
-//    };
-//  }
-//
-//  private CsrfTokenRepository csrfTokenRepository() {
-//    HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-//    repository.setHeaderName("X-XSRF-TOKEN");
-//    return repository;
-//  }
 
 
   @Autowired
